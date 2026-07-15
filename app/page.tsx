044d01858e559c7202c5d089867329819808693c
Grupo Lisea <grupolisea.corp@gmail.com>
2026-07-14 21:12:03 -0600

    feat: activar redirección automática al login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
-      <h1>¡Mascara Lisea está en línea!</h1>
-    </div>
-  );
+  // Redirige automáticamente al usuario a la pantalla de login al entrar a la web
+  redirect("/login");
 }
