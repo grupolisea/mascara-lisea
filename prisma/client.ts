b9b6644fec4a86943056278ac7fcbd62087ab771
Grupo Lisea <grupolisea.corp@gmail.com>
2026-07-15 14:04:55 -0600

    fix: simplificacion de singleton prisma para evitar errores de tipo

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,14 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// En Vercel, es crítico definir el datasource explícitamente
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+// Si ya existe una instancia, la usamos; si no, creamos una nueva.
+// En entornos Serverless, Prisma gestiona internamente la conexión.
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
