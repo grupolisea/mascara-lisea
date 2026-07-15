f1c862eef60b218a04de5cadece2908d27a058ad
Grupo Lisea <grupolisea.corp@gmail.com>
2026-07-15 09:25:24 -0600

    fix: configurar constructor de prisma manualmente

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,10 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Usamos la configuración estándar. 
-// Prisma lee automáticamente process.env.DATABASE_URL
+// Forzamos el uso de la conexión estándar sin intentar usar el nuevo "adapter" o "accelerate"
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['error'], 
+  log: ['error'],
+  // Esta línea es la que soluciona el error de "engine type"
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
 
 if (process.env.NODE_ENV !== "production") {
