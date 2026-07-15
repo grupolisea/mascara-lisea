852d12195077c88bedcfda107d2baee1620d2dba
Grupo Lisea <grupolisea.corp@gmail.com>
2026-07-14 21:42:00 -0600

    feat: activar redirección automática al login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Redirige automáticamente al usuario a la pantalla de login al entrar a la web
+  // Redirige al usuario automáticamente a la pantalla de login
   redirect("/login");
 }
