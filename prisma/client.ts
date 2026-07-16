9f5ee132b33b3521253e8a70763fdfc0ddc135af
Grupo Lisea <grupolisea.corp@gmail.com>
2026-07-15 21:55:05 -0600

    fix: build final con prisma adapter y configuración optimizada

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+// Usamos un Singleton para evitar múltiples conexiones en serverless
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Si ya existe una instancia, la usamos; si no, creamos una nueva.
-// En entornos Serverless, Prisma gestiona internamente la conexión.
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// Definimos el pool de conexión para que Prisma lo utilice como adaptador
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
